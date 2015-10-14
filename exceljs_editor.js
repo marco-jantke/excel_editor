a7cf96d821dad91cd59d8f9b2f9a33908ff42d6e
Iuliia Snizhkovska <iuliia.snizhkovska@mayflower.de>
2015-10-14 14:30:41 +0200

    remove closures

--- exceljs_editor.js
+++ exceljs_editor.js
@@ -1,59 +1,56 @@
-(function () {
+var csvHelper = require('./csvHelper.js');
+var VendorReportObj = require('./VendorReportObject.js');
+var Excel = require("./node_modules/exceljs/excel.js");
+var workbook = new Excel.Workbook();
 
-    var csvHelper = require('./csvHelper.js');
-    var VendorReportObj = require('./VendorReportObject.js');
-    var Excel = require("./node_modules/exceljs/excel.js");
-    var workbook = new Excel.Workbook();
+var getRowData = function getRowData(worksheet, rowNumber) {
+    var row = worksheet.getRow(rowNumber);
+    var vendorObj = new VendorReportObj(row.getCell(1).value, row.getCell(2).value, row.getCell(3).value, row.getCell(4).value,
+        row.getCell(5).value, row.getCell(6).value, row.getCell(7).value, row.getCell(8).value,
+        row.getCell(9).value, row.getCell(10).value, row.getCell(11).value, row.getCell(12).value,
+        row.getCell(13).value);
+    return vendorObj;
+};
 
-    var getRowData = function getRowData(worksheet, rowNumber) {
-        var row = worksheet.getRow(rowNumber);
-        var vendorObj = new VendorReportObj(row.getCell(1).value, row.getCell(2).value, row.getCell(3).value, row.getCell(4).value,
-            row.getCell(5).value, row.getCell(6).value, row.getCell(7).value, row.getCell(8).value,
-            row.getCell(9).value, row.getCell(10).value, row.getCell(11).value, row.getCell(12).value,
-            row.getCell(13).value);
-        return vendorObj;
-    };
-
-    var addNewRow = function addNewRow(worksheet, vendorObj) {
-        var cells = [];
-        for (var prop in vendorObj) {
-            if (typeof(vendorObj[prop]) !== "function") {
-                cells.push(vendorObj[prop]);
-            }
+var addNewRow = function addNewRow(worksheet, vendorObj) {
+    var cells = [];
+    for (var prop in vendorObj) {
+        if (typeof(vendorObj[prop]) !== "function") {
+            cells.push(vendorObj[prop]);
         }
-        worksheet.addRow(cells);
-    };
+    }
+    worksheet.addRow(cells);
+};
 
-    var readDataFromCsvFileAndWriteToXlsx = function readDataFromCsvFileAndWriteToXlsx(pathCsv, pathXlsx) {
-        var arr = [];
-        var p = workbook.csv.readFile(pathCsv)
-            .then(function(worksheet) {
-                worksheet.eachRow(function(row, rowNumber) {
-                    if(rowNumber !== 1) {
-                        csvHelper.addRowToArray(row.values, arr);
-                    }
-                });
-                arr = csvHelper.convertReceivedDataToObjectsArr(arr);
-            }).then( function() {
-                writeXlsxFile(arr, pathXlsx);
-            });
-    };
-
-    var writeXlsxFile = function writeXlsxFile(readData, path) {
-        workbook.xlsx.readFile(path)
-            .then(function () {
-                var worksheet = workbook.getWorksheet(2);
-                for (var i = 0; i < readData.length; i++) {
-                    readData[i].setCostsByHourPrice(worksheet.getCell("L1").value, worksheet.getCell("K1").value);
-                    addNewRow(worksheet, readData[i]);
+var readDataFromCsvFileAndWriteToXlsx = function readDataFromCsvFileAndWriteToXlsx(pathCsv, pathXlsx) {
+    var arr = [];
+    workbook.csv.readFile(pathCsv)
+        .then(function (worksheet) {
+            worksheet.eachRow(function (row, rowNumber) {
+                if (rowNumber !== 1) {
+                    csvHelper.addRowToArray(row.values, arr);
                 }
-                workbook.xlsx.writeFile("./excelData/ven_rep_exceljs_editor.xlsx");
             });
-    };
+            arr = csvHelper.convertReceivedDataToObjectsArr(arr);
+        }).then(function () {
+            writeXlsxFile(arr, pathXlsx);
+        });
+};
 
-    /** ENTRY POINT */
-    readDataFromCsvFileAndWriteToXlsx('./excelData/project_stat.csv', './excelData/vendor_reporting_09_18.xlsx');
-})();
+var writeXlsxFile = function writeXlsxFile(readData, path) {
+    workbook.xlsx.readFile(path)
+        .then(function () {
+            var worksheet = workbook.getWorksheet(2);
+            for (var i = 0; i < readData.length; i++) {
+                readData[i].setCostsByHourPrice(worksheet.getCell("L1").value, worksheet.getCell("K1").value);
+                addNewRow(worksheet, readData[i]);
+            }
+            workbook.xlsx.writeFile("./excelData/ven_rep_exceljs_editor.xlsx");
+        });
+};
+
+/** ENTRY POINT */
+readDataFromCsvFileAndWriteToXlsx('./excelData/project_stat.csv', './excelData/vendor_reporting_09_18.xlsx');
 
 
 
--- xlsx_editor.js
+++ xlsx_editor.js
@@ -1,40 +1,38 @@
-(function () {
-    var VendorReportObj = require('./VendorReportObject.js');
-    var XLSXWorkbookObj = require('./XLSXWorkbookObject.js');
-    var csvHelper = require('./csvHelper.js');
-    var csv = require('csv');
-    var fs = require('fs');
+var VendorReportObj = require('./VendorReportObject.js');
+var XLSXWorkbookObj = require('./XLSXWorkbookObject.js');
+var csvHelper = require('./csvHelper.js');
+var csv = require('csv');
+var fs = require('fs');
 
-    /** This function take path to csvFile with data you want to append to xlsxFile, and path to xlsxFile*/
-    var main = function main(csvFileToRead , xlsxFileToRead) {
-        var workbook = new XLSXWorkbookObj(xlsxFileToRead, 1);
-        var readData = [];
+/** This function take path to csvFile with data you want to append to xlsxFile, and path to xlsxFile*/
+var main = function main(csvFileToRead, xlsxFileToRead) {
+    var workbook = new XLSXWorkbookObj(xlsxFileToRead, 1);
+    var readData = [];
 
-        csv()
-            .from.stream(fs.createReadStream(csvFileToRead))
-            .to.path('./excelData/text.txt')
-            .transform(function (row) {
-                row.unshift(row.pop());
-                return row;
-            })
-            .on('record', function (row, index) {
-                /** hear we read data from csv file row by row and put it into array readData*/
-                if (index !== 0) {
-                    csvHelper.addRowToArray(row, readData);
-                }
-            })
-            .on('end', function (count) {
-                var vendorRepObjects = csvHelper.convertReceivedDataToObjectsArr(readData);
-                workbook.addNewObjectsToTheWorksheet(vendorRepObjects, workbook.getWorksheetHeaders());
-                workbook.increaseWorksheetRange(vendorRepObjects.length);
-                //console.log(workbook.getWorksheet());
-                workbook.writeWorkbookToTheFile('./excelData/ven_rep_xlsx_editor.xlsx');
-            })
-            .on('error', function (error) {
-                console.log(error.message);
-            });
-    };
+    csv()
+        .from.stream(fs.createReadStream(csvFileToRead))
+        .to.path('./excelData/text.txt')
+        .transform(function (row) {
+            row.unshift(row.pop());
+            return row;
+        })
+        .on('record', function (row, index) {
+            /** hear we read data from csv file row by row and put it into array readData*/
+            if (index !== 0) {
+                csvHelper.addRowToArray(row, readData);
+            }
+        })
+        .on('end', function () {
+            var vendorRepObjects = csvHelper.convertReceivedDataToObjectsArr(readData);
+            workbook.addNewObjectsToTheWorksheet(vendorRepObjects, workbook.getWorksheetHeaders());
+            workbook.increaseWorksheetRange(vendorRepObjects.length);
+            //console.log(workbook.getWorksheet());
+            workbook.writeWorkbookToTheFile('./excelData/ven_rep_xlsx_editor.xlsx');
+        })
+        .on('error', function (error) {
+            console.log(error.message);
+        });
+};
 
-    /** THIS IS ENTRY POINT*/
-    main('./excelData/project_stat.csv' ,'./excelData/vendor_reporting_09_18.xlsx');
-})();
+/** THIS IS ENTRY POINT*/
+main('./excelData/project_stat.csv', './excelData/vendor_reporting_09_18.xlsx');
